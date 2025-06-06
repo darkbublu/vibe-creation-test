b30eb56c95d9402dde9180f1fb4e5a166f7c6894
darkbublu <davidcyprianhicks@gmail.com>
2025-06-06 15:44:52 -0700

    File operations via console - 2025-06-06T22:44:51.896Z

---
+++ test-1749249560408-src/ui-components/Button.tsx
@@ -0,0 +1,8 @@
+---
+id: 2453bcc9-2453-2453-2453-2453bcc9
+path: test-1749249560408-src/ui-components/Button.tsx
+level: file
+title: Button
+description: React TypeScript component - Auto-generated
+---
+# Moved file
---
+++ test-1749249560408-src/ui-components/Input.tsx
@@ -0,0 +1,8 @@
+---
+id: 2891f7a5-2891-2891-2891-2891f7a5
+path: test-1749249560408-src/ui-components/Input.tsx
+level: file
+title: Input
+description: React TypeScript component - Auto-generated
+---
+# Moved file
