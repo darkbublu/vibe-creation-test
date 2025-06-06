92bf5aa7144856659f850ac18477ca178956838d
darkbublu <davidcyprianhicks@gmail.com>
2025-06-06 00:15:21 -0700

    Delete debug-test-folder/index.js

--- debug-test-folder/index.js
+++
@@ -1,9 +0,0 @@
----
-id: 56101c65-5610-5610-5610-56101c65
-path: debug-test-folder/index.js
-level: module
-title: Index
-description: JavaScript module - Auto-generated
----
-// debug-test-folder main file
-console.log("Hello from debug-test-folder!");
