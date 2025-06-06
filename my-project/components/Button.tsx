2d45f41d9462ee967574d566eb87acd5c6a53f84
darkbublu <davidcyprianhicks@gmail.com>
2025-06-06 15:34:33 -0700

    Delete my-project/components/Button.tsx

--- my-project/components/Button.tsx
+++
@@ -1,8 +0,0 @@
----
-id: 770c4d01-770c-770c-770c-770c4d01
-path: my-project/components/Button.tsx
-level: file
-title: Button
-description: React TypeScript component - Auto-generated
----
-# Moved file
