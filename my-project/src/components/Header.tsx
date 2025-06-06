1e9c9b83639a652d7e33c762756ed5622dce7ffe
darkbublu <davidcyprianhicks@gmail.com>
2025-06-06 12:01:42 -0700

    Delete my-project/src/components/Header.tsx

--- my-project/src/components/Header.tsx
+++
@@ -1,8 +0,0 @@
----
-id: 4cf7b591-4cf7-4cf7-4cf7-4cf7b591
-path: my-project/src/components/Header.tsx
-level: file
-title: Header
-description: React TypeScript component - Auto-generated
----
-Header component
