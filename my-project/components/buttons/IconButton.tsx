bad582552ec354fcf6667cb669ea1b673a022cf6
darkbublu <davidcyprianhicks@gmail.com>
2025-06-06 15:28:40 -0700

    Move my-project/components/buttons/IconButton.tsx to ui/components/buttons/IconButton.tsx (cleanup)

--- my-project/components/buttons/IconButton.tsx
+++
@@ -1,8 +0,0 @@
----
-id: 05a955a6-05a9-05a9-05a9-05a955a6
-path: my-project/components/buttons/IconButton.tsx
-level: file
-title: IconButton
-description: React TypeScript component - Auto-generated
----
-# Moved file
