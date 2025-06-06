8a48e356bdfd39b0c8ae963a09340f40756a3760
darkbublu <davidcyprianhicks@gmail.com>
2025-06-06 16:32:06 -0700

    Create test-1749252555542-src/ui-components/Button.tsx

---
+++ test-1749252555542-src/ui-components/Button.tsx
@@ -0,0 +1,9 @@
+---
+id: 2e8830d2-2e88-2e88-2e88-2e8830d2
+path: test-1749252555542-src/ui-components/Button.tsx
+level: file
+title: Button
+description: React TypeScript component - Auto-generated
+---
+// Button Component
+export const Button = () => <button>Click me</button>;
