29e503b14668d9e62045a54c56e9558f3724fd09
darkbublu <davidcyprianhicks@gmail.com>
2025-06-06 11:29:20 -0700

    Create my-project/src/components/Button.tsx

---
+++ my-project/src/components/Button.tsx
@@ -0,0 +1,8 @@
+---
+id: 5288b494-5288-5288-5288-5288b494
+path: my-project/src/components/Button.tsx
+level: file
+title: Button
+description: React TypeScript component - Auto-generated
+---
+Button component
