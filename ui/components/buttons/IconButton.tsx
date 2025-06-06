24fd7f2b2af6ecc6d0064446ddf891e2baf14fc4
darkbublu <davidcyprianhicks@gmail.com>
2025-06-06 15:28:39 -0700

    Move my-project/components/buttons/IconButton.tsx to ui/components/buttons/IconButton.tsx

---
+++ ui/components/buttons/IconButton.tsx
@@ -0,0 +1,10 @@
+---
+id: 66d412be-66d4-66d4-66d4-66d412be
+path: ui/components/buttons/IconButton.tsx
+level: file
+title: IconButton
+description: React TypeScript component - Auto-generated
+---
+# Moved File
+
+This file was moved.
