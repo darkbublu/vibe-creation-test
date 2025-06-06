8883b00436d203c42a0e172f22b74a993aee8584
darkbublu <davidcyprianhicks@gmail.com>
2025-06-05 22:28:36 -0700

    Quick test: Create folder-name

---
+++ folder-name/index.js
@@ -0,0 +1,9 @@
+---
+id: 09bacfca-09ba-09ba-09ba-09bacfca
+path: folder-name/index.js
+level: module
+title: Index
+description: JavaScript module - Auto-generated
+---
+// Quick test file
+console.log("Hello from folder-name!");
