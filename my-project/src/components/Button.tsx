d440dddf878b490c9ed50f26f37dd6b2c5f15860
darkbublu <davidcyprianhicks@gmail.com>
2025-06-06 12:01:42 -0700

    Delete my-project/src/components/Button.tsx

--- my-project/src/components/Button.tsx
+++
@@ -1,8 +0,0 @@
----
-id: 5288b494-5288-5288-5288-5288b494
-path: my-project/src/components/Button.tsx
-level: file
-title: Button
-description: React TypeScript component - Auto-generated
----
-Button component
