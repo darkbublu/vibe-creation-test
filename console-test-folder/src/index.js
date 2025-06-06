0e38812226f3a193e6c79c26a927955a09f31e26
darkbublu <davidcyprianhicks@gmail.com>
2025-06-05 22:29:11 -0700

    Create console-test-folder/src/index.js

---
+++ console-test-folder/src/index.js
@@ -0,0 +1,9 @@
+---
+id: 1453bdac-1453-1453-1453-1453bdac
+path: console-test-folder/src/index.js
+level: file
+title: Index
+description: JavaScript module - Auto-generated
+---
+// Console test file
+console.log("Hello from console created file!");
