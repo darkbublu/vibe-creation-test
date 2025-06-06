eb55411a3e108e3713721578ef1ab97bc57f64c0
darkbublu <davidcyprianhicks@gmail.com>
2025-06-06 15:15:13 -0700

    File operations via console - 2025-06-06T22:15:13.375Z

--- my-project/components/Button.tsx
+++ my-project/components/Button.tsx
@@ -1,6 +1,6 @@
 ---
-id: 01382b65-0138-0138-0138-01382b65
-path: ui/components/Button.tsx
+id: 770c4d01-770c-770c-770c-770c4d01
+path: my-project/components/Button.tsx
 level: file
 title: Button
 description: React TypeScript component - Auto-generated
