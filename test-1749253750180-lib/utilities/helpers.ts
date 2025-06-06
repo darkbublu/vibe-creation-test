8ffddff67edb94a947200c3f8576d1fa02904e95
darkbublu <davidcyprianhicks@gmail.com>
2025-06-06 16:52:46 -0700

    File operations via console - 2025-06-06T23:52:46.101Z

---
+++ test-1749253750180-lib/utilities/helpers.ts
@@ -0,0 +1,8 @@
+---
+id: 4b788e23-4b78-4b78-4b78-4b788e23
+path: test-1749253750180-lib/utilities/helpers.ts
+level: file
+title: Helpers
+description: TypeScript module - Auto-generated
+---
+# Moved file
--- test-1749253750180-src/utils/helpers.ts
+++
@@ -1,9 +0,0 @@
----
-id: 1b9a3063-1b9a-1b9a-1b9a-1b9a3063
-path: test-1749253750180-src/utils/helpers.ts
-level: file
-title: Helpers
-description: TypeScript module - Auto-generated
----
-// Helper functions
-export const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
