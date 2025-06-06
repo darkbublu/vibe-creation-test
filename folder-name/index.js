94a954ed2bd91f0878ab347acf346b25da100c7e
darkbublu <davidcyprianhicks@gmail.com>
2025-06-06 11:33:58 -0700

    Delete folder-name directory

--- folder-name/index.js
+++
@@ -1,9 +0,0 @@
----
-id: 09bacfca-09ba-09ba-09ba-09bacfca
-path: folder-name/index.js
-level: module
-title: Index
-description: JavaScript module - Auto-generated
----
-// Quick test file
-console.log("Hello from folder-name!");
