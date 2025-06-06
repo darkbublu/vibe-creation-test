94207aa9f2d2f80e0c317158070ed00830ca43d6
darkbublu <davidcyprianhicks@gmail.com>
2025-06-06 16:31:44 -0700

    Create test-1749252555542-src/utils/helpers.ts

---
+++ test-1749252555542-src/utils/helpers.ts
@@ -0,0 +1,9 @@
+---
+id: 15d08c89-15d0-15d0-15d0-15d08c89
+path: test-1749252555542-src/utils/helpers.ts
+level: file
+title: Helpers
+description: TypeScript module - Auto-generated
+---
+// Helper functions
+export const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
