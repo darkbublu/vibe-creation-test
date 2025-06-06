d67c18089c458350dbf4b013f17324d8756188bc
darkbublu <davidcyprianhicks@gmail.com>
2025-06-05 22:29:12 -0700

    Create console-test-folder/src/utils.js

---
+++ console-test-folder/src/utils.js
@@ -0,0 +1,9 @@
+---
+id: 392fd56d-392f-392f-392f-392fd56d
+path: console-test-folder/src/utils.js
+level: file
+title: Utils
+description: JavaScript module - Auto-generated
+---
+// Utility functions
+export const greet = (name) => "Hello, " + name + "!";
