9545a4edd64e41196ab02ad66564829e8852c291
darkbublu <davidcyprianhicks@gmail.com>
2025-06-06 16:32:06 -0700

    Delete test-1749252555542-src/components/Button.tsx

--- test-1749252555542-src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
----
-id: 0ba673a5-0ba6-0ba6-0ba6-0ba673a5
-path: test-1749252555542-src/components/Button.tsx
-level: file
-title: Button
-description: React TypeScript component - Auto-generated
----
-// Button Component
-export const Button = () => <button>Click me</button>;
